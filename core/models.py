c6111b7d467234d9b8892a78ed66b03ec5ef1c00
Manikanta <manikantanallamati03@gmail.com>
2025-07-11 10:20:17 +0530

    Started Project

---
+++ core/models.py
@@ -0,0 +1,60 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Video(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    file = models.FileField(upload_to='videos/')
+    thumbnail = models.ImageField(upload_to='thumbnails/', blank=True, null=True)
+    uploaded_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='uploaded_videos')
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+    views = models.IntegerField(default=0)
+    liked_by = models.ManyToManyField(User, related_name='liked_videos', blank=True)
+    category = models.CharField(max_length=100, blank=True, null=True)
+
+    def __str__(self):
+        return self.title
+
+    def total_likes(self):
+        return self.liked_by.count()
+
+
+class Comment(models.Model):
+    video = models.ForeignKey(Video, on_delete=models.CASCADE, related_name='comments')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    comment = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} on {self.video.title}"
+
+
+class UserFollowing(models.Model):
+    user = models.ForeignKey(User, related_name='following', on_delete=models.CASCADE)
+    following_user = models.ForeignKey(User, related_name='followers', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('user', 'following_user')
+
+    def __str__(self):
+        return f"{self.user.username} follows {self.following_user.username}"
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True)
+    avatar = models.ImageField(upload_to='avatars/', blank=True, null=True)
+    is_private = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.user.username
+
+
+class UserSettings(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    dark_mode = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.user.username}'s settings"
